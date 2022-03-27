4ca081a7f8c7cbf9a38c389d7b98b7f441b41ab1
asold2 <304147@via.dk>
2022-03-27 19:54:28 +0200

    BinarySearchTreeNode

---
+++ src/BinarySearchTreeNode.java
@@ -0,0 +1,11 @@
+public class BinarySearchTreeNode<E> extends BinaryTreeNode implements Comparable{
+
+//    private E element;
+//    private BinaryTreeNode leftChild;
+//    private BinaryTreeNode rightChild;
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
