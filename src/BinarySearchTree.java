4a915cbca3ad7168b379fe457f375896716789eb
asold2 <304147@via.dk>
2022-03-27 19:59:05 +0200

    skeleton for BinarySearchTree

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,27 @@
+public class BinarySearchTree<E> extends BinaryTree{
+    private BinarySearchTreeNode root;
+
+    public boolean insert(E element){
+        return true;
+    }
+
+    public boolean removeElement(E element){
+        return true;
+    }
+
+    public E findMin(){
+        return null;
+    }
+
+    public E findMax(){
+        return null;
+    }
+
+    public void rebalance(){
+
+    }
+
+
+}
+
+
