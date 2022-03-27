2d598002b010e7498fb040d72cc76b4aa701c499
asold2 <304147@via.dk>
2022-03-27 19:37:21 +0200

    BinaryTreeNode created

---
+++ src/BinaryTreeNode.java
@@ -0,0 +1,39 @@
+public class BinaryTreeNode<E> {
+    private E element;
+    private BinaryTreeNode leftChild;
+    private BinaryTreeNode rightChild;
+
+
+    public void setElement(E element){
+        this.element = element;
+    }
+    public E getElement(){
+        return element;
+    }
+    public void addLeftChild(BinaryTreeNode leftChild){
+        this.leftChild = leftChild;
+    }
+    public void addRightChild(BinaryTreeNode rightChild){
+        this.rightChild = rightChild;
+    }
+
+    public BinaryTreeNode getLeftChild(){
+        if(leftChild!=null){
+            return leftChild;
+        }
+        else{
+            return null;
+        }
+    }
+
+    public BinaryTreeNode getRightChild(){
+        if(rightChild!=null){
+            return rightChild;
+        }
+        else{
+            return null;
+        }
+    }
+
+
+}
